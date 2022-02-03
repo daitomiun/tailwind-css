f9e2d74af89175a94e0f9e4716de7966f30e76e6
Mateo Rodriguez <mateo.rodriguez.fierro@gmail.com>
2022-02-03 16:29:03 -0500

    avance, colores personalizados no funcionan por alguna razon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  media: false,
+
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#CC2D4A",
+      secondary: "#8FA206",
+      terciary: "#61AEC9",
+    }),
+    textcolor: (theme) => ({
+      ...theme("colors"),
+      primary: "#CC2D4A",
+      secondary: "#8FA206",
+      terciary: "#61AEC9",
+    }),
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        norway: "url('../img/norway.jpg')",
+        newYork: "url('../img/new_york.jpg')",
+        miami: "url('../img/miami.jpg')",
+        LA: "url('../img/LA.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+        europe: "url('../img/europe.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        bali: "url('../img/bali.jpg')",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
